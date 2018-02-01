17efc2bca41deed7bde9af596a5c685064f70a29
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2018-02-01 10:06:47 +0100

    Added button select sound

---
+++ Assets/Scripts/UISound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISound : StateMachineBehaviour {
+    public AudioClip clip;
+
+    void OnStateEnter() {
+        // Get the audio source attached to the main camera
+        AudioSource audioSource = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+        audioSource.PlayOneShot(clip);
+    }
+}
