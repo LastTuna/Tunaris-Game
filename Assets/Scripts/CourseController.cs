0b14947f002a56ebdc2799d11f9c9419c348e30a
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2018-02-02 09:42:10 +0100

    fix course controller npe whn menu is bypassed

--- Assets/Scripts/CourseController.cs
+++ Assets/Scripts/CourseController.cs
@@ -6,12 +6,15 @@ public class CourseController : MonoBehaviour {
     public GameData settings;
 
 	void Start () {
-        settings = GameObject.Find("DataController").GetComponent<DataController>().LoadedData;
+        GameObject dataController = GameObject.Find("DataController");
+        if (dataController != null) {
+            settings = dataController.GetComponent<DataController>().LoadedData;
 
-        // This is an example of how to access data set by the UI in the course scene
-        // Sadly, it only works in C# because of a compile order snafu
-        // So this controller may have to do all the heavy lifting around the unityscript car controller
-        // btw, unityscript has been officially deprecated so you should really move to C#. really.
-        Debug.Log(settings.SelectedCar);
+            // This is an example of how to access data set by the UI in the course scene
+            // Sadly, it only works in C# because of a compile order snafu
+            // So this controller may have to do all the heavy lifting around the unityscript car controller
+            // btw, unityscript has been officially deprecated so you should really move to C#. really.
+            Debug.Log(settings.SelectedCar);
+        }
     }
 }
