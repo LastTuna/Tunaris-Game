29d8b626b476dd77f40f6728d6bc7804f3b757c0
LastTuna <Tunaisthebest@gmail.com>
2018-02-05 11:45:34 +0200

    ported start race scriptto c
    e

---
+++ Assets/cars/raceStart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raceStart : MonoBehaviour {
+    public AudioSource beep;
+    public AudioSource launch;
+    // Use this for initialization
+    void Start () {
+       StartCoroutine(CountDown());
+
+	}
+
+    IEnumerator CountDown ()
+    {
+        yield return new WaitForSeconds(1.0f);
+        beep.Play();
+        yield return new WaitForSeconds(1.0f);
+        beep.Play();
+        yield return new WaitForSeconds(1.0f);
+        launch.Play();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
