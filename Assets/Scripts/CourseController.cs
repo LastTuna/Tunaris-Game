86386c582dff9d31fafc670e609a1e3a699fec7c
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2018-02-07 22:04:08 +0100

    Added first drafts of CourseController handling MP session creation and TGNetworkManager extending Unity's built in NetworkManager

--- Assets/Scripts/CourseController.cs
+++ Assets/Scripts/CourseController.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CourseController : MonoBehaviour {
+    // Settings passed from the menus
     public GameData settings;
+
+    // Are we loading into multiplayer
+    public bool IsMultiplayer = true;
+    // Server IP, null if hosting
+    public string IP = null;
+    // Multiplayer prefab to instantiate, should contain shit like spawn points
+    public GameObject MultiplayerPrefab;
 
 	void Start () {
         GameObject dataController = GameObject.Find("DataController");
@@ -15,6 +23,19 @@ public class CourseController : MonoBehaviour {
             // So this controller may have to do all the heavy lifting around the unityscript car controller
             // btw, unityscript has been officially deprecated so you should really move to C#. really.
             Debug.Log(settings.SelectedCar);
+
+            // Get multiplayer-related data
+            // IsMultiplayer = settings.IsMultiplayer;
+            // IP = settings.IP;
+        }
+
+        // Initialize network-related resources
+        if (IsMultiplayer) {
+            // Create all the spawn points
+            GameObject MpPrefab = Instantiate(MultiplayerPrefab);
+
+            // Start hosting
+            MpPrefab.GetComponentInChildren<TGNetworkManager>().StartHost();
         }
     }
 }
---
+++ Assets/Scripts/TGNetworkManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TGNetworkManager : NetworkManager {
+    public List<GameObject> cars;
+    public List<GameObject> spawnPositions;
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId) {
+        GameObject player = Instantiate(cars[0], spawnPositions[0].transform.position, Quaternion.identity);
+        player.AddComponent<NetworkIdentity>().localPlayerAuthority = true;
+        player.AddComponent<NetworkTransform>();
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+}
