7291e5198b32f7c77185cf08a475a90239b5cf4b
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2018-02-09 11:04:11 +0100

    Refactored car behaviours

--- Assets/Scripts/CarBehaviour.cs
+++ Assets/Scripts/CarBehaviour.cs
@@ -1,10 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-using UnityEngine.Networking;
 
-public class merci : MonoBehaviour
-{
+public class CarBehaviour : MonoBehaviour {
+
 
     public Text speedDisplay;//output of speed to meter - by default MPH
     public Text gearDisplay;
@@ -26,6 +26,17 @@ public class merci : MonoBehaviour
     public float aero = 15.0f; //aero - higher value = higher grip, but less accel/topspeed
     public float ratio; //final drive
     public float tyreBias = 0.6f; //tyre bias - smaller value = offroad - higher value = tarmac (0-1) BASE TIRE GRIP VALUE = 1
+    /// <summary>
+    /// How much power is being sent to the front wheels, as a ratio, can be used to change drivetrain
+    /// 0: no power to front, 100% power to rear
+    /// 0.5: half front, half rear
+    /// 1: 100% front,  no rear
+    /// </summary>
+    public float FrontWheelDriveBias = 0.5f;
+    // Having it as a ratio opens a whole lot of tricks, but mainly an easy way to allocate power for ALL drivetrains
+    // FrontPower = engineOutput * FrontWheelDriveBias
+    // RearPower = engineOutput * (1-FrontWheelDriveBias)
+    // Chaning this while the car is driving is an effective way of having a center diff
 
 
     //end tuneable stats
@@ -45,9 +56,8 @@ public class merci : MonoBehaviour
     public int gear;//current gear
     public bool shifting = false;//shifter delay
 
-    void Start()
-    {
-        ValueChecks();
+    void Start() {
+        HUDUpdate();
         engineRPM = 800;
         ratio = 4.3f;
         Physics.gravity = new Vector3(0, -aero, 0);
@@ -58,8 +68,7 @@ public class merci : MonoBehaviour
 
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         StartCoroutine(engine());
 
         if (Input.GetAxis("Handbrake") > 0f)//HANDBRAKE
@@ -71,23 +80,18 @@ public class merci : MonoBehaviour
 
             wheelRL.brakeTorque = 300;
             wheelRR.brakeTorque = 300;
-        }
-        else
-        {
+        } else {
             wheelRL.brakeTorque = 0;
             wheelRR.brakeTorque = 0;
         }
 
 
-        if (Input.GetAxis("Brake") > 0f)
-        {//brakes
+        if (Input.GetAxis("Brake") > 0f) {//brakes
             wheelFL.brakeTorque = brakeStrength;
             wheelFR.brakeTorque = brakeStrength;
             wheelRL.brakeTorque = brakeStrength;
             wheelRR.brakeTorque = brakeStrength;
-        }
-        else
-        {
+        } else {
             wheelFL.brakeTorque = 0;
             wheelFR.brakeTorque = 0;
             wheelRL.brakeTorque = 0;
@@ -102,10 +106,8 @@ public class merci : MonoBehaviour
         currentSpeed = Mathf.Round(currentSpeed);
     }
 
-    WheelFrictionCurve SetStiffness(WheelFrictionCurve current, float newStiffness)
-    {
-        return new WheelFrictionCurve()
-        {
+    WheelFrictionCurve SetStiffness(WheelFrictionCurve current, float newStiffness) {
+        return new WheelFrictionCurve() {
             asymptoteSlip = wheelRL.sidewaysFriction.asymptoteSlip,
             asymptoteValue = wheelRL.sidewaysFriction.asymptoteValue,
             extremumSlip = wheelRL.sidewaysFriction.extremumSlip,
@@ -114,12 +116,10 @@ public class merci : MonoBehaviour
         };
     }
 
-    void Update()
-    {
-        
+    void Update() {
         StartCoroutine(gearbox());//gearbox update 
-        ValueChecks();
-        Offroad();
+        HUDUpdate();
+        SetSurfaceProperties();
         wheelFRTrans.Rotate(wheelFL.rpm / 60 * 360 * Time.deltaTime, 0, 0); //graphical updates
         wheelFLTrans.Rotate(wheelFL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
         wheelRRTrans.Rotate(wheelFL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
@@ -129,26 +129,17 @@ public class merci : MonoBehaviour
         WheelPosition(); //graphical update - wheel positions 
     }
 
-    IEnumerator engine()
-    {//engine
-        if (gear == 1)
-        {//neutral revs
+    IEnumerator engine() {//engine
+        if (gear == 1) {//neutral revs
 
-            if (engineRPM >= engineREDLINE)
-            {
+            if (engineRPM >= engineREDLINE) {
                 engineRPM = engineRPM - 300;
-            }
-            else
-            {
-                if (Input.GetAxis("Throttle") > 0)
-                {
+            } else {
+                if (Input.GetAxis("Throttle") > 0) {
                     yield return new WaitForSeconds(0.1f);
                     engineRPM = 100 + engineRPM;
-                }
-                else
-                {
-                    if (engineRPM > 800)
-                    {
+                } else {
+                    if (engineRPM > 800) {
                         yield return new WaitForSeconds(0.05f);
                         engineRPM = engineRPM - 100;
                     }
@@ -156,42 +147,39 @@ public class merci : MonoBehaviour
 
 
             }
-        }
-        else
-        { //drive revs
+        } else { //drive revs
             engineRPM = wheelRPM * gears[gear] * ratio + 800;
         }
 
-        if (gear > 0)
-        {
+        if (gear > 0) {
             unitOutput = (engineRPM / 1000) * (engineRPM / 1000) + 120; //ENGINE OUTPUT TO WHEELS
-        }
-        else
-        {
+        } else {
             unitOutput = -(engineRPM / 1000) * (engineRPM / 1000) - 120; //reverse output
         }
-        if (engineRPM > engineREDLINE || gear == 1 || Input.GetAxis("Throttle") < 0)
-        {//throttle & rev limit
-            wheelFR.motorTorque = 0;
-            wheelFL.motorTorque = 0;
-        }
-        else
-        {
-            wheelFR.motorTorque = unitOutput * Input.GetAxis("Throttle");
-            wheelFL.motorTorque = unitOutput * Input.GetAxis("Throttle");
+        if (engineRPM > engineREDLINE || gear == 1 || Input.GetAxis("Throttle") < 0) {//throttle & rev limit
+            wheelFR.motorTorque = 0 * FrontWheelDriveBias;
+            wheelFL.motorTorque = 0 * FrontWheelDriveBias;
+
+            wheelRR.motorTorque = 0 * (1 - FrontWheelDriveBias);
+            wheelRL.motorTorque = 0 * (1 - FrontWheelDriveBias);
+        } else { 
+            wheelFR.motorTorque = unitOutput * Input.GetAxis("Throttle") * FrontWheelDriveBias;
+            wheelFL.motorTorque = unitOutput * Input.GetAxis("Throttle") * FrontWheelDriveBias;
+
+            wheelRR.motorTorque = unitOutput * Input.GetAxis("Throttle") * (1 - FrontWheelDriveBias);
+            wheelRL.motorTorque = unitOutput * Input.GetAxis("Throttle") * (1 - FrontWheelDriveBias);
         }
     }
-    IEnumerator gearbox()
-    {
-        if (Input.GetButtonDown("ShiftUp") == true && gear < 7 && shifting == false)
-        {
+
+    // Gearbox managed, called each frame
+    IEnumerator gearbox() {
+        if (Input.GetButtonDown("ShiftUp") == true && gear < 7 && shifting == false) {
             shifting = true;
             gear = gear + 1;
             yield return new WaitForSeconds(0.3f);
             shifting = false;
         }
-        if (Input.GetButtonDown("ShiftDown") == true && gear > 0 && shifting == false)
-        {
+        if (Input.GetButtonDown("ShiftDown") == true && gear > 0 && shifting == false) {
             shifting = true;
             gear = gear - 1;
             yield return new WaitForSeconds(0.1f);
@@ -199,121 +187,88 @@ public class merci : MonoBehaviour
         }
     }
 
-    void WheelPosition()
-    { //graphical update - wheel positions 
+    // Graphical update - wheel positions 
+    void WheelPosition() { 
         RaycastHit hit;
         Vector3 wheelPos;
         //FL
-        if (Physics.Raycast(wheelFL.transform.position, -wheelFL.transform.up, out hit, wheelFL.radius + wheelFL.suspensionDistance))
-        {
+        if (Physics.Raycast(wheelFL.transform.position, -wheelFL.transform.up, out hit, wheelFL.radius + wheelFL.suspensionDistance)) {
             wheelPos = hit.point + wheelFL.transform.up * wheelFL.radius;
-        }
-        else
-        {
+        } else {
             wheelPos = wheelFL.transform.position - wheelFL.transform.up * wheelFL.suspensionDistance;
         }
         wheelFLTrans.position = wheelPos;
         //FR
-        if (Physics.Raycast(wheelFR.transform.position, -wheelFR.transform.up, out hit, wheelFR.radius + wheelFR.suspensionDistance))
-        {
+        if (Physics.Raycast(wheelFR.transform.position, -wheelFR.transform.up, out hit, wheelFR.radius + wheelFR.suspensionDistance)) {
             wheelPos = hit.point + wheelFR.transform.up * wheelFR.radius;
-        }
-        else
-        {
+        } else {
             wheelPos = wheelFR.transform.position - wheelFR.transform.up * wheelFR.suspensionDistance;
         }
         wheelFRTrans.position = wheelPos;
         //RL
-        if (Physics.Raycast(wheelRL.transform.position, -wheelRL.transform.up, out hit, wheelRL.radius + wheelRL.suspensionDistance))
-        {
+        if (Physics.Raycast(wheelRL.transform.position, -wheelRL.transform.up, out hit, wheelRL.radius + wheelRL.suspensionDistance)) {
             wheelPos = hit.point + wheelRL.transform.up * wheelRL.radius;
-        }
-        else
-        {
+        } else {
             wheelPos = wheelRL.transform.position - wheelRL.transform.up * wheelRL.suspensionDistance;
         }
         wheelRLTrans.position = wheelPos;
         //RR
-        if (Physics.Raycast(wheelRR.transform.position, -wheelRR.transform.up, out hit, wheelRR.radius + wheelRR.suspensionDistance))
-        {
+        if (Physics.Raycast(wheelRR.transform.position, -wheelRR.transform.up, out hit, wheelRR.radius + wheelRR.suspensionDistance)) {
             wheelPos = hit.point + wheelRR.transform.up * wheelRR.radius;
-        }
-        else
-        {
+        } else {
             wheelPos = wheelRR.transform.position - wheelRR.transform.up * wheelRR.suspensionDistance;
         }
         wheelRRTrans.position = wheelPos;
     }
 
-    void Offroad()
-    {
+    // Sets the surface properties, based on what the front left (?) wheel is doing
+    // Called each frame
+    void SetSurfaceProperties() {
         WheelHit FLhit;
         float wheelpreload;
 
-        //4444444444444444444444444444444
-        if (wheelFL.GetGroundHit(out FLhit))
-        {
-            if (FLhit.collider.gameObject.CompareTag("sand"))
-            {
-                if (currentGrip > 1.1f)
-                {
+        // Ground surface detection
+        if (wheelFL.GetGroundHit(out FLhit)) {
+            if (FLhit.collider.gameObject.CompareTag("sand")) {
+                if (currentGrip > 1.1f) {
                     currentGrip = 1.1f;
-                }
-                else
-                {
+                } else {
                     currentGrip = 1 - tyreBias + 0.3f;//OFFROAD
                 }
-                wheelFR.sidewaysFriction = SetStiffness(wheelFR.sidewaysFriction, currentGrip + 0.1f);
-                wheelFL.sidewaysFriction = SetStiffness(wheelFL.sidewaysFriction, currentGrip + 0.1f);
-                wheelFR.forwardFriction = SetStiffness(wheelFL.forwardFriction, currentGrip + 0.1f);
-                wheelFL.forwardFriction = SetStiffness(wheelFL.forwardFriction, currentGrip + 0.1f);
-                //
-                wheelRR.sidewaysFriction = SetStiffness(wheelRR.sidewaysFriction, currentGrip);
-                wheelRL.sidewaysFriction = SetStiffness(wheelRL.sidewaysFriction, currentGrip);
-                wheelRR.forwardFriction = SetStiffness(wheelRL.forwardFriction, currentGrip);
-                wheelRL.forwardFriction = SetStiffness(wheelRL.forwardFriction, currentGrip);
-            }
-            else
-            {
-                if(currentGrip > 1.3f)
-                {
-                currentGrip = 1.3f;
-                }
-                else
-                {
+                // On sand, wheelpreload was set to 0.1f as constant
+                wheelpreload = 0.1f;
+            } else {
+                if (currentGrip > 1.3f) {
+                    currentGrip = 1.3f;
+                } else {
                     currentGrip = 1 * tyreBias + 0.4f;//TARMAC
                 }
-                if(currentGrip > 0.9f)//drive wheel preload - provides extra grip to drive wheels.
+                if (currentGrip > 0.9f)//drive wheel preload - provides extra grip to drive wheels.
                 {//make sure to change application accordingly!!!! (RWD, FWD, AWD)
                     wheelpreload = 0f; //this setting applies to TARMAC ONLY!!! change offroad value accordingly
-                }
-                else
-                {
+                } else {
                     wheelpreload = 0.2f;
                 }
-                wheelFR.sidewaysFriction = SetStiffness(wheelFR.sidewaysFriction, currentGrip + wheelpreload);
-                wheelFL.sidewaysFriction = SetStiffness(wheelFL.sidewaysFriction, currentGrip + wheelpreload);
-                wheelFR.forwardFriction = SetStiffness(wheelFL.forwardFriction, currentGrip + wheelpreload);
-                wheelFL.forwardFriction = SetStiffness(wheelFL.forwardFriction, currentGrip + wheelpreload);
-                //
-                wheelRR.sidewaysFriction = SetStiffness(wheelRR.sidewaysFriction, currentGrip);
-                wheelRL.sidewaysFriction = SetStiffness(wheelRL.sidewaysFriction, currentGrip);
-                wheelRR.forwardFriction = SetStiffness(wheelRL.forwardFriction, currentGrip);
-                wheelRL.forwardFriction = SetStiffness(wheelRL.forwardFriction, currentGrip);
             }
-            
-            
+            // Those are always called, might as well take them out of the if blocks
+            // Front wheels
+            wheelFR.sidewaysFriction = SetStiffness(wheelFR.sidewaysFriction, currentGrip + (wheelpreload * FrontWheelDriveBias));
+            wheelFL.sidewaysFriction = SetStiffness(wheelFL.sidewaysFriction, currentGrip + (wheelpreload * FrontWheelDriveBias));
+            wheelFR.forwardFriction = SetStiffness(wheelFL.forwardFriction, currentGrip + (wheelpreload * FrontWheelDriveBias));
+            wheelFL.forwardFriction = SetStiffness(wheelFL.forwardFriction, currentGrip + (wheelpreload * FrontWheelDriveBias));
+            // Rear wheels
+            wheelRR.sidewaysFriction = SetStiffness(wheelRR.sidewaysFriction, currentGrip + (wheelpreload * (1 - FrontWheelDriveBias)));
+            wheelRL.sidewaysFriction = SetStiffness(wheelRL.sidewaysFriction, currentGrip + (wheelpreload * (1 - FrontWheelDriveBias)));
+            wheelRR.forwardFriction = SetStiffness(wheelRL.forwardFriction, currentGrip + (wheelpreload * (1 - FrontWheelDriveBias)));
+            wheelRL.forwardFriction = SetStiffness(wheelRL.forwardFriction, currentGrip + (wheelpreload * (1 - FrontWheelDriveBias)));
         }
     }
 
-
-
-    void ValueChecks()
-    {
+    // Updates the HUD
+    void HUDUpdate() {
         float speedFactor = engineRPM / engineREDLINE;//dial rotation
         float rotationAngle = 0;
-        if (engineRPM >= 0)
-        {
+        if (engineRPM >= 0) {
             rotationAngle = Mathf.Lerp(70, -160, speedFactor);
             pointer.eulerAngles = new Vector3(0, 0, rotationAngle);
         }//end dial rot
@@ -321,25 +276,16 @@ public class merci : MonoBehaviour
         if (currentSpeed < 0)//cancelling negative integers, speed
         {
             speedDisplay.text = (currentSpeed * -1).ToString();
-        }
-        else
-        {
+        } else {
             speedDisplay.text = currentSpeed.ToString();
         }
-        if(gear == 0)//gears
+        if (gear == 0)//gears
         {
             gearDisplay.text = "R".ToString();//reverse gear
-        }
-        else if(gear == 1)
-        {
+        } else if (gear == 1) {
             gearDisplay.text = "N".ToString();//neutral
-        }
-        else
-        {
+        } else {
             gearDisplay.text = (gear - 1).ToString();//array value, minus 1
         }
-
-
     }
 }
-
