7772ad39f82eeb434ed792dec9f9a671ec206bd1
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2018-04-27 10:56:27 +0200

    car camera script

--- Assets/Scripts/CarCamera.cs
+++ Assets/Scripts/CarCamera.cs
@@ -11,19 +11,46 @@ public class CarCamera : MonoBehaviour {
     public float DefaultFOV = 60;
     private Vector3 rotationVector;
 
+    // Variable to store the selected camera
+    public int chosenCamera = 0;
+
     void LateUpdate() {
         if (car != null) {
-            float wantedAngel = rotationVector.y;
-            float wantedHeight = car.position.y + height;
-            float myAngel = transform.eulerAngles.y;
-            float myHeight = transform.position.y;
-            myAngel = Mathf.LerpAngle(myAngel, wantedAngel, rotationDamping * Time.deltaTime);
-            myHeight = Mathf.Lerp(myHeight, wantedHeight, heightDamping * Time.deltaTime);
-            Quaternion currentRotation = Quaternion.Euler(0, myAngel, 0);
-            transform.position = car.position;
-            transform.position -= currentRotation * Vector3.forward * distance;
-            transform.position = new Vector3(transform.position.x, myHeight, transform.position.z);
-            transform.LookAt(car);
+            switch (chosenCamera) {
+                // 0, default, 3rd person cam
+                case 0: {
+                        float wantedAngel = rotationVector.y;
+                        float wantedHeight = car.position.y + height;
+                        float myAngel = transform.eulerAngles.y;
+                        float myHeight = transform.position.y;
+                        myAngel = Mathf.LerpAngle(myAngel, wantedAngel, rotationDamping * Time.deltaTime);
+                        myHeight = Mathf.Lerp(myHeight, wantedHeight, heightDamping * Time.deltaTime);
+                        Quaternion currentRotation = Quaternion.Euler(0, myAngel, 0);
+                        transform.position = car.position;
+                        transform.position -= currentRotation * Vector3.forward * distance;
+                        transform.position = new Vector3(transform.position.x, myHeight, transform.position.z);
+                        transform.LookAt(car);
+
+                        car.Find("Cockpit").gameObject.SetActive(false);
+                    }
+                    break;
+                // 1: cockpit cam
+                case 1: {
+                        transform.position = car.position;
+                        transform.position += Vector3.up * 0.5f;
+                        transform.rotation = car.rotation;
+                        
+                        car.Find("Cockpit").gameObject.SetActive(true);
+                    }
+                    break;
+                default: break;
+            }
+        }
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Camera")) {
+            chosenCamera = (chosenCamera < 1) ? chosenCamera + 1 : 0;
         }
     }
 
