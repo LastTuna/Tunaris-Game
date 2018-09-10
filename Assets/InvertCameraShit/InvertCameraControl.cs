be9791e5cb470db3e5d26d31dc98143b30f145a0
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2018-09-10 14:42:32 +0200

    Negative colors mod, thank you tes

---
+++ Assets/InvertCameraShit/InvertCameraControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertCameraControl : MonoBehaviour {
+    public GameObject InvertCameraObject;
+    void Update() {
+        if (Input.GetButtonDown("InvertColors")) {
+            InvertCameraObject.SetActive(!InvertCameraObject.activeSelf);
+        }
+    }
+}
