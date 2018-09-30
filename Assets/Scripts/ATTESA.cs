b7fa676e2c40501e39cbbb57f17b487979f77fbb
LastTuna <Tunaisthebest@gmail.com>
2018-10-01 00:03:38 +0300

    ATTESA now features active all wheel drive. implement STSC tomorrow

--- Assets/Scripts/ATTESA.cs
+++ Assets/Scripts/ATTESA.cs
@@ -8,6 +8,7 @@ public class ATTESA : MonoBehaviour {
     public WheelCollider wheelFR;
     public WheelCollider wheelRL;
     public WheelCollider wheelRR;
+    public CarBehaviour carBehaviour;
 
     // Use this for initialization
     void Start () {
@@ -19,9 +20,10 @@ public class ATTESA : MonoBehaviour {
     {
         WheelHit wheelhitL;
         WheelHit wheelhitR;
+        //detect slip for rear wheels and steer accordingly
         if (wheelRL.GetGroundHit(out wheelhitL) && wheelRR.GetGroundHit(out wheelhitR))
         {
-            if(wheelhitL.sidewaysSlip > 0.05f || wheelhitR.sidewaysSlip > 0.05f)
+            if(SlipAvg(wheelhitL, wheelhitR) > 0.05f)
             {
                 wheelRL.steerAngle = SlipAvg(wheelhitL, wheelhitR) * 30 + ( - wheelFL.steerAngle / 6);
                 wheelRR.steerAngle = SlipAvg(wheelhitL, wheelhitR) * 30 + ( - wheelFR.steerAngle / 6);
@@ -31,12 +33,18 @@ public class ATTESA : MonoBehaviour {
                 wheelRL.steerAngle = -wheelFL.steerAngle / 6;
                 wheelRR.steerAngle = -wheelFR.steerAngle / 6;
             }
+            ActiveDifferential(wheelhitL, wheelhitR);
         }
     }
 
     float SlipAvg(WheelHit wheelL, WheelHit wheelR)
     {
-        return (wheelL.sidewaysSlip + wheelR.sidewaysSlip) / 2; ;
+        return (wheelL.sidewaysSlip + wheelR.sidewaysSlip) / 2;
+    }
+
+    void ActiveDifferential(WheelHit wheelL, WheelHit wheelR)
+    {
+        carBehaviour.FrontWheelDriveBias = Mathf.Abs(wheelL.forwardSlip + wheelR.forwardSlip);
     }
 
 }
