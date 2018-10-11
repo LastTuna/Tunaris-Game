f739eea5a7e2b2927ce0d3986b055b8b7ab88cbb
LastTuna <Tunaisthebest@gmail.com>
2018-10-12 00:48:23 +0300

    windshield wipers(still borked)

--- Assets/Scripts/Wipers.cs
+++ Assets/Scripts/Wipers.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Wipers : MonoBehaviour {
     public bool wiping = false;
-
+    public float speed;
+    public float angle;
+    public bool despacito;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,9 +14,35 @@ public class Wipers : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (wiping)
+        {
+            Wiper();
+        }
 
-        //wipe
+
     }
+    void Wiper()
+    {
+        if (angle >= 90)
+        {
+            despacito = false;
+        }
+        if (angle <= 0)
+        {
+            despacito = true;
+        }
+        ////
+        if (despacito)
+        {
+            angle += speed;
+            gameObject.transform.localRotation *= Quaternion.AngleAxis(speed, new Vector3(0, 1, 0));
+        }
+        else
+        {
+            angle += -speed;
+            gameObject.transform.localRotation *= Quaternion.AngleAxis(speed, new Vector3(0, -1, 0));
+        }
 
+    }
     
 }
