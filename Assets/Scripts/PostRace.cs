dc622b56e2dc6fa4f3dabeb2d10c411d61e4f408
BuildTools <343ays@gmail.com>
2018-11-06 22:22:27 -0200

    fixed the dumb code i put, needs testing!!

--- Assets/Scripts/PostRace.cs
+++ Assets/Scripts/PostRace.cs
@@ -35,7 +35,7 @@ public class PostRace : MonoBehaviour {
     IEnumerator TallyLaps()
     {
         checkum = false;
-        while (count <= laps)
+        while (count < laps)
         {
             
             Debug.Log(count);
