52192ae2de0bf305632d33641c3eb4e0a5cfad69
LastTuna <Tunaisthebest@gmail.com>
2018-11-08 20:41:25 +0200

    Revert "b r e a k"
    This reverts commit 0a43fc8ee144aaab235a38a86f72a71818141d4c.

--- Assets/Scripts/CarBehaviour.cs
+++ Assets/Scripts/CarBehaviour.cs
@@ -35,7 +35,6 @@ public class CarBehaviour : NetworkBehaviour {
     public float currentSpeed;
     public float wheelRPM;
     public float engineRPM;
-    public float unitOutput;
     public float turboSpool = 0.1f;//turbo boost value
     public bool spooled = false;//determine whether to play wastegate sound or not
     public int gear;//current gear
@@ -104,6 +103,7 @@ public class CarBehaviour : NetworkBehaviour {
             ratio = dataController.FinalDrive;
             manual = true;
             dirtiness = dataController.Dirtiness[carIndex];
+            TGNetworkManager networkmanager = FindObjectOfType<TGNetworkManager>();
             //spring stiffness set (dampers = spring / 10)
             springs.spring = springStiffness;
             springs.damper = springStiffness / 10;
@@ -164,10 +164,9 @@ public class CarBehaviour : NetworkBehaviour {
 
     void Sparker()
     {
-        if (Input.GetKeyDown("p") && !runnin)
+        if (Input.GetKeyDown("p"))
         {
-            engineCrank.motorTorque = 700;
-            runnin = true;
+            engineCrank.motorTorque = 30;
         }
 
     }
@@ -182,11 +181,7 @@ public class CarBehaviour : NetworkBehaviour {
             if(engineRPM < 1000)
             {
                 //idle
-                engineCrank.motorTorque = 30;
-            }
-            if (engineRPM < 500)
-            {
-                runnin = false;
+                engineCrank.motorTorque = 10;
             }
         }
         if(engineRPM > engineREDLINE)
@@ -196,15 +191,6 @@ public class CarBehaviour : NetworkBehaviour {
         }
         engineRPM = engineCrank.rpm;
         
-
-
-
-
-
-
-
-
-
         //turbo manager
         if (engineRPM > 830 && Input.GetAxis("Throttle") > 0)
         {//when you step on the gas
@@ -244,7 +230,7 @@ public class CarBehaviour : NetworkBehaviour {
 
     void Differential()
     {
-        wheelFL.motorTorque = engineTorque.Evaluate(engineRPM) * gears[gear];
+
     }
     //* ratio / gears[gear]
     //engineTorque.Evaluate(engineRPM) 
