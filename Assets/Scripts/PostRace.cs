bdea1f59ff1a4edcd2a071459fb19e9c9158fe62
Danny Walovsch <Tunaisthebest@gmail.com>
2018-11-12 12:28:57 +0200

    rather instantiate than hardcode laptimes

--- Assets/Scripts/PostRace.cs
+++ Assets/Scripts/PostRace.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +6,15 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PostRace : MonoBehaviour {
+    
     public bool checkum;
     public int laps;
     public int count;
-    public static Text[] laptime = new Text[10];
-
-
+    public GameObject textInstance;
+    public static GameObject[] textInstances;
+    public Vector3 spawnPos;
     public List<TimeSpan> lapTally = new List<TimeSpan>();
+
     // Use this for initialization
     void Start ()
     {
@@ -28,6 +29,7 @@ public class PostRace : MonoBehaviour {
 
         if (GameObject.Find("0") && checkum)
         {
+
             StartCoroutine(TallyLaps());
         }
 	}
@@ -35,41 +37,25 @@ public class PostRace : MonoBehaviour {
     IEnumerator TallyLaps()
     {
         checkum = false;
-        while (count < laps)
-        {
-            
-            Debug.Log(count);
+        Debug.Log(count);
+        float raise = 0;
+        int i = 0;
+            foreach(TimeSpan e in lapTally)
+            {
+            textInstances[i] = Instantiate(textInstance, new Vector3(spawnPos.x, spawnPos.y + raise, 0), new Quaternion(0, 0, 0, 0));
+            textInstances[i].GetComponent<Text>().text = e.ToString();
+            raise += 30;
+            yield return new WaitForSeconds(0.1f);
+            }
             // To do:
             // Make a dynamic lap tally (this WILL crash if it's above 10 laps!!!)
+            //achieve by using instantiate() and make a prefab with text element
             // Add "Best Lap" option @ 
             // make fucking unity stop sperging about null exceptions despite all laps being shown lol
-            laptime[count] = GameObject.Find(Convert.ToString(count)).GetComponent<Text>();
-            laptime[count].text = string.Format("{0:00}:{1:00}:{2:000}", lapTally[count].Minutes, lapTally[count].Seconds, lapTally[count].Milliseconds); 
-            count++;
-        }
+
+            //laptime[count] = GameObject.Find(Convert.ToString(count)).GetComponent<Text>();
+            //laptime[count].text = string.Format("{0:00}:{1:00}:{2:000}", lapTally[count].Minutes, lapTally[count].Seconds, lapTally[count].Milliseconds); 
         
-        // Old code lol
-        //laptime1 = GameObject.Find("lap1").GetComponent<Text>();
-        //laptime2 = GameObject.Find("lap2").GetComponent<Text>();
-        //laptime3 = GameObject.Find("lap3").GetComponent<Text>();
-        //laptime4 = GameObject.Find("lap4").GetComponent<Text>();
-        //laptime5 = GameObject.Find("lap5").GetComponent<Text>();
-        //laptime6 = GameObject.Find("lap6").GetComponent<Text>();
-        //laptime7 = GameObject.Find("lap7").GetComponent<Text>();
-        //laptime8 = GameObject.Find("lap8").GetComponent<Text>();
-        //laptime9 = GameObject.Find("lap9").GetComponent<Text>();
-        // laptime10 = GameObject.Find("lap10").GetComponent<Text>();
-        //laptime1.text = string.Format("{0:00}:{1:00}:{2:000}", lapTally[0].Minutes, lapTally[0].Seconds, lapTally[0].Milliseconds);
-        //laptime2.text = string.Format("{0:00}:{1:00}:{2:000}", lapTally[1].Minutes, lapTally[1].Seconds, lapTally[1].Milliseconds);
-        //laptime3.text = string.Format("{0:00}:{1:00}:{2:000}", lapTally[2].Minutes, lapTally[2].Seconds, lapTally[2].Milliseconds);
-        //laptime4.text = string.Format("{0:00}:{1:00}:{2:000}", lapTally.Min().Minutes, lapTally.Min().Seconds, lapTally.Min().Milliseconds);
-        //laptime5.text = "null";
-        //laptime6.text = "null";
-        //laptime7.text = "null";
-        //laptime8.text = "null";
-        //laptime9.text = "null";
-        //laptime10.text = "will be fixed later";
-        //laptime1.text = string.Format("{0:00}:{1:00}:{2:000}", laptally[0].Minutes, laptimes[0].Seconds, laptimes[0].Milliseconds);
         yield return new WaitForSecondsRealtime(4);
         SceneManager.LoadScene(0, LoadSceneMode.Single);
 
