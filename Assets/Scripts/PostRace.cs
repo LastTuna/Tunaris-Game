1313cfe905820d026bce144099e9049b1b9b892a
BuildTools <343ays@gmail.com>
2018-11-15 00:07:49 -0200

    git pisses me off, fucks up code; test it and idk what got broke, on trip and i'll re-do my crap when im back. pls test

--- Assets/Scripts/PostRace.cs
+++ Assets/Scripts/PostRace.cs
@@ -5,28 +5,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Linq;
-<<<<<<< HEAD
-
-public class PostRace : MonoBehaviour {
-    
-=======
 
 public class PostRace : MonoBehaviour
 {
-
->>>>>>> master
     public bool checkum;
     public int laps;
     public int count;
     public GameObject textInstance;
     public List<GameObject> textInstances = new List<GameObject>();
-<<<<<<< HEAD
     public Vector3 spawnPos;
-=======
-    public Vector2 spawnPos;
->>>>>>> master
     public List<TimeSpan> lapTally = new List<TimeSpan>();
     public Canvas mainCanvas;
+    public ScrollRect lapScroll;
+    public RectTransform LapTimes;
 
     // Use this for initialization
     void Start()
@@ -44,6 +35,8 @@ public class PostRace : MonoBehaviour
         if (GameObject.Find("0") && checkum)
         {
             mainCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+            lapScroll = GameObject.Find("ScrollBar").GetComponent<ScrollRect>();
+            LapTimes = GameObject.Find("LapTimes").GetComponent<RectTransform>();
             StartCoroutine(TallyLaps());
         }
     }
@@ -51,34 +44,30 @@ public class PostRace : MonoBehaviour
     IEnumerator TallyLaps()
     {
         checkum = false;
-        float raise = 0;//raise next element by x amount
+        float raise = 80;//raise next element by x amount
+        count = lapTally.Count - 1;
         GameObject despacito;
-<<<<<<< HEAD
-        foreach(TimeSpan e in lapTally)
-=======
         foreach (TimeSpan e in lapTally)
->>>>>>> master
         {
             despacito = null;//clears object reference
             despacito = Instantiate(textInstance, new Vector3(spawnPos.x, spawnPos.y + raise, 0), new Quaternion(0, 0, 0, 0), mainCanvas.transform);
             //create new laptime object/element
             despacito.GetComponent<Text>().text = string.Format("{0:00}:{1:00}:{2:000}", lapTally[count].Minutes, lapTally[count].Seconds, lapTally[count].Milliseconds);
-            if (e.Equals(lapTally.Min()))
-            {//bestest lap time
-                despacito.GetComponent<Text>().text += " BEST";
-<<<<<<< HEAD
-            }
-=======
-                despacito.GetComponent<Text>().color = new Color(200,0,0);
+            //if (e.Equals(lapTally.Min()))
+            //{//bestest lap time
+                //despacito.GetComponent<Text>().text += " BEST";
+				//despacito.GetComponent<Text>().color = new Color(200,0,0);
+				// dataController.BestestLapTimes[0] = string.Format("{0:00}:{1:00}.{2:000}", lapTally[count].Minutes, lapTally[count].Seconds, lapTally[count].Milliseconds);
+            //}
+                
                 //DataController dataController = FindObjectOfType<DataController>();
                 //dataController.BestestLapTimes[0] = e;
                 //may be borked, havent tried yet. essentially saves best lap time to gamesave
-            }
             despacito.transform.position = new Vector3(spawnPos.x, spawnPos.y + raise, 0);
->>>>>>> master
+            despacito.transform.parent = LapTimes.transform;
             textInstances.Add(despacito);//add all elements to list (will use later)
             raise += 30;//raise by x amount
-            count++;//tally/loop
+            count--;//tally/loop
             yield return new WaitForSeconds(0.1f);
             Debug.Log(count);
         }
@@ -91,7 +80,6 @@ public class PostRace : MonoBehaviour
         // Make a dynamic lap tally (this WILL crash if it's above 10 laps!!!) - DONE!
         //achieve by using instantiate() and make a prefab with text element - DONE!
         // Add "Best Lap" option @ - DONE
-<<<<<<< HEAD
 
 
         //laptime[count] = GameObject.Find(Convert.ToString(count)).GetComponent<Text>();
@@ -100,18 +88,5 @@ public class PostRace : MonoBehaviour
         yield return new WaitForSecondsRealtime(4);
         //SceneManager.LoadScene(0, LoadSceneMode.Single);
         //disabled scene load for easier debugging
-        
-=======
-
-
-        //laptime[count] = GameObject.Find(Convert.ToString(count)).GetComponent<Text>();
-        //laptime[count].text = string.Format("{0:00}:{1:00}:{2:000}", lapTally[count].Minutes, lapTally[count].Seconds, lapTally[count].Milliseconds); 
-
-        yield return new WaitForSecondsRealtime(4);
-        SceneManager.LoadScene(0, LoadSceneMode.Single);
-        //disabled scene load for easier debugging
-
->>>>>>> master
-
     }
 }
