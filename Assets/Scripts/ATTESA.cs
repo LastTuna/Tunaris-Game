f16760981eb019691686a2fd094dc07ba134ca46
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2019-09-20 15:01:45 +0200

    fixed assists by: putting them in fixedupdate disabling the reset when no hits because that happens often

--- Assets/Scripts/ATTESA.cs
+++ Assets/Scripts/ATTESA.cs
@@ -3,58 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ATTESA : MonoBehaviour {
-
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
     public WheelCollider wheelRL;
     public WheelCollider wheelRR;
     public CarBehaviour carBehaviour;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate() {
         WheelHit wheelhitL;
         WheelHit wheelhitR;
         //detect slip for rear wheels and steer accordingly
-        if (wheelRL.GetGroundHit(out wheelhitL) && wheelRR.GetGroundHit(out wheelhitR))
-        {
-            if (Mathf.Abs(SlipAvg(wheelhitL, wheelhitR)) > 0.05f)
-            {
-                wheelRL.steerAngle = SlipAvg(wheelhitL, wheelhitR) * 30 + ( - wheelFL.steerAngle / 6);
-                wheelRR.steerAngle = SlipAvg(wheelhitL, wheelhitR) * 30 + ( - wheelFR.steerAngle / 6);
-            }
-            else
-            {
+        if (wheelRL.GetGroundHit(out wheelhitL) && wheelRR.GetGroundHit(out wheelhitR)) {
+            if (Mathf.Abs(SlipAvg(wheelhitL, wheelhitR)) > 0.05f) {
+                wheelRL.steerAngle = SlipAvg(wheelhitL, wheelhitR) * 30 + (-wheelFL.steerAngle / 6);
+                wheelRR.steerAngle = SlipAvg(wheelhitL, wheelhitR) * 30 + (-wheelFR.steerAngle / 6);
+            } else {
                 wheelRL.steerAngle = -wheelFL.steerAngle / 6;
                 wheelRR.steerAngle = -wheelFR.steerAngle / 6;
             }
             ActiveDifferential(wheelhitL, wheelhitR);
-        }
-        else
-        {
-            wheelRL.steerAngle = 0;
-            wheelRR.steerAngle = 0;
+        } else {
+            //wheelRL.steerAngle = 0;
+            //wheelRR.steerAngle = 0;
         }
     }
 
-    float SlipAvg(WheelHit wheelL, WheelHit wheelR)
-    {
+    float SlipAvg(WheelHit wheelL, WheelHit wheelR) {
         return (wheelL.sidewaysSlip + wheelR.sidewaysSlip) / 2;
     }
 
-    void ActiveDifferential(WheelHit wheelL, WheelHit wheelR)
-    {
-        if(wheelL.forwardSlip + wheelR.forwardSlip > 0.5f)
-        {
+    void ActiveDifferential(WheelHit wheelL, WheelHit wheelR) {
+        if (wheelL.forwardSlip + wheelR.forwardSlip > 0.5f) {
             carBehaviour.FrontWheelDriveBias = 0.5f;
-        }
-        else
-        {
+        } else {
             carBehaviour.FrontWheelDriveBias = Mathf.Abs(wheelL.forwardSlip + wheelR.forwardSlip);
         }
     }
--- Assets/Scripts/STCSAWD.cs
+++ Assets/Scripts/STCSAWD.cs
@@ -3,47 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class STCSAWD : MonoBehaviour {
-
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
     public WheelCollider wheelRL;
     public WheelCollider wheelRR;
     public CarBehaviour carBehaviour;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate() {
         WheelHit wheelhitL;
         WheelHit wheelhitR;
         //detect slip for rear wheels and steer accordingly
-        if (wheelRL.GetGroundHit(out wheelhitL) && wheelRR.GetGroundHit(out wheelhitR))
-        {
-            if (Mathf.Abs(SlipAvg(wheelhitL, wheelhitR)) > 0.05f)
-            {
+        if (wheelRL.GetGroundHit(out wheelhitL) && wheelRR.GetGroundHit(out wheelhitR)) {
+            if (Mathf.Abs(SlipAvg(wheelhitL, wheelhitR)) > 0.05f) {
                 wheelRL.steerAngle = SlipAvg(wheelhitL, wheelhitR) * 30 + (-wheelFL.steerAngle / 6);
                 wheelRR.steerAngle = SlipAvg(wheelhitL, wheelhitR) * 30 + (-wheelFR.steerAngle / 6);
-            }
-            else
-            {
+            } else {
                 wheelRL.steerAngle = -wheelFL.steerAngle / 6;
                 wheelRR.steerAngle = -wheelFR.steerAngle / 6;
             }
-        }
-        else
-        {
-            wheelRL.steerAngle = 0;
-            wheelRR.steerAngle = 0;
+        } else {
+            //wheelRL.steerAngle = 0;
+            //wheelRR.steerAngle = 0;
         }
     }
 
-    float SlipAvg(WheelHit wheelL, WheelHit wheelR)
-    {
+    float SlipAvg(WheelHit wheelL, WheelHit wheelR) {
         return (wheelL.sidewaysSlip + wheelR.sidewaysSlip) / 2;
     }
 
