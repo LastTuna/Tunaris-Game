344ea2eea33a5dd699ffdae7817310e317737591
Franck Dechavanne <dechavanne.franck@hotmail.fr>
2020-02-24 15:34:53 +0100

    poopy shader update most of the files are probs useless except StandardInput but i did renames and cant be arsed scene edited with special gtr for easy testing

---
+++ Assets/Shaders/testdirt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testdirt : MonoBehaviour {
+	public float dirtvalue = 0f;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (dirtvalue >= 0f && dirtvalue <= 1f) {
+			GetComponent<Renderer>().material.SetFloat("_FortniteRange", dirtvalue);
+		}
+	}
+}
